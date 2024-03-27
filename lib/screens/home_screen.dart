330e7199f56c3196f78a4680fb30068f945570f5
Lans12345 <olanalans12345@gmail.com>
2024-03-28 06:54:34 +0800

    fetched user name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:grave_finder/widgets/drawer_widget.dart';
 import 'package:photo_view/photo_view.dart';
@@ -14,150 +16,167 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final searchController = TextEditingController();
   String nameSearched = '';
+  final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+      .collection('Users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .snapshots();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        drawer: const DrawerWidget(),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          title: TextWidget(
-            text: 'Welcome, John Doe',
-            fontSize: 18,
-            fontFamily: 'Bold',
-          ),
-        ),
-        body: Stack(
-          children: [
-            PhotoView(
-              imageProvider: const AssetImage(
-                  'assets/images/CementeryMap-SanLuisMemorialPark.png'),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 20),
-              child: Column(
+    return StreamBuilder<DocumentSnapshot>(
+        stream: userData,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const SizedBox();
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SizedBox();
+          }
+          dynamic data = snapshot.data;
+          return Scaffold(
+              drawer: const DrawerWidget(),
+              appBar: AppBar(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+                title: TextWidget(
+                  text: '${'Welcome, ' + data['fname']} ',
+                  fontSize: 18,
+                  fontFamily: 'Bold',
+                ),
+              ),
+              body: Stack(
                 children: [
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      height: 40,
-                      width: 350,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.white,
+                  PhotoView(
+                    imageProvider: const AssetImage(
+                        'assets/images/CementeryMap-SanLuisMemorialPark.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: Container(
+                            height: 40,
+                            width: 350,
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.white,
+                                ),
+                                borderRadius: BorderRadius.circular(100)),
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 10, right: 10),
+                              child: TextFormField(
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontFamily: 'Regular',
+                                    fontSize: 14),
+                                onChanged: (value) {
+                                  setState(() {
+                                    nameSearched = value;
+                                  });
+                                },
+                                decoration: const InputDecoration(
+                                    labelStyle: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                    hintText: 'Search',
+                                    hintStyle: TextStyle(
+                                      fontFamily: 'QRegular',
+                                      color: Colors.white,
+                                    ),
+                                    prefixIcon: Icon(
+                                      Icons.search,
+                                      color: Colors.white,
+                                    )),
+                                controller: searchController,
+                              ),
+                            ),
                           ),
-                          borderRadius: BorderRadius.circular(100)),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        child: TextFormField(
-                          style: const TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Regular',
-                              fontSize: 14),
-                          onChanged: (value) {
-                            setState(() {
-                              nameSearched = value;
-                            });
-                          },
-                          decoration: const InputDecoration(
-                              labelStyle: TextStyle(
-                                color: Colors.white,
-                              ),
-                              hintText: 'Search',
-                              hintStyle: TextStyle(
-                                fontFamily: 'QRegular',
-                                color: Colors.white,
-                              ),
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.white,
-                              )),
-                          controller: searchController,
                         ),
-                      ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextWidget(
+                          text: 'Legend',
+                          fontSize: 18,
+                          fontFamily: 'Bold',
+                          color: Colors.white,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 75,
+                                  color: Colors.green,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text: 'Available',
+                                  fontSize: 12,
+                                  fontFamily: 'Bold',
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 75,
+                                  color: Colors.yellow,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text: 'Pre-Reserved',
+                                  fontSize: 12,
+                                  fontFamily: 'Bold',
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  height: 50,
+                                  width: 75,
+                                  color: Colors.red,
+                                ),
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                TextWidget(
+                                  text: 'Occupied',
+                                  fontSize: 12,
+                                  fontFamily: 'Bold',
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextWidget(
-                    text: 'Legend',
-                    fontSize: 18,
-                    fontFamily: 'Bold',
-                    color: Colors.white,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 75,
-                            color: Colors.green,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          TextWidget(
-                            text: 'Available',
-                            fontSize: 12,
-                            fontFamily: 'Bold',
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 75,
-                            color: Colors.yellow,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          TextWidget(
-                            text: 'Pre-Reserved',
-                            fontSize: 12,
-                            fontFamily: 'Bold',
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 75,
-                            color: Colors.red,
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          TextWidget(
-                            text: 'Occupied',
-                            fontSize: 12,
-                            fontFamily: 'Bold',
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
                 ],
-              ),
-            ),
-          ],
-        ));
+              ));
+        });
   }
 }
--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:grave_finder/screens/login_page.dart';
 import 'package:grave_finder/widgets/text_widget.dart';
@@ -10,122 +12,139 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: 250,
-      color: background,
-      child: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  width: 20,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: primary),
-                      shape: BoxShape.circle,
-                      color: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.all(2.5),
-                    child: Image.asset(
-                      'assets/images/profile.png',
-                      height: 35,
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+    return StreamBuilder<DocumentSnapshot>(
+        stream: userData,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const SizedBox();
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SizedBox();
+          }
+          dynamic data = snapshot.data;
+          return Container(
+            height: double.infinity,
+            width: 250,
+            color: background,
+            child: SafeArea(
+                child: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: primary),
+                            shape: BoxShape.circle,
+                            color: Colors.white),
+                        child: Padding(
+                          padding: const EdgeInsets.all(2.5),
+                          child: Image.asset(
+                            'assets/images/profile.png',
+                            height: 35,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      TextWidget(
+                        text: data['fname'] + ' ' + data['lname'],
+                        fontFamily: 'Bold',
+                        fontSize: 16,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                    },
+                    title: TextWidget(
+                      text: 'Map',
+                      fontSize: 14,
+                      fontFamily: 'Bold',
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                TextWidget(
-                  text: 'John Doe',
-                  fontFamily: 'Bold',
-                  fontSize: 16,
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const HomeScreen()));
-              },
-              title: TextWidget(
-                text: 'Map',
-                fontSize: 14,
-                fontFamily: 'Bold',
+                  ListTile(
+                    onTap: () {
+                      // Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      //     builder: (context) => const HomeScreen()));
+                    },
+                    title: TextWidget(
+                      text: 'Navigation',
+                      fontSize: 14,
+                      fontFamily: 'Bold',
+                    ),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                                title: const Text(
+                                  'Logout Confirmation',
+                                  style: TextStyle(
+                                      fontFamily: 'QBold',
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                content: const Text(
+                                  'Are you sure you want to Logout?',
+                                  style: TextStyle(fontFamily: 'QRegular'),
+                                ),
+                                actions: <Widget>[
+                                  MaterialButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text(
+                                      'Close',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () async {
+                                      Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const LoginPage()));
+                                    },
+                                    child: const Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ));
+                    },
+                    title: TextWidget(
+                      text: 'Logout',
+                      fontSize: 14,
+                      fontFamily: 'Bold',
+                    ),
+                  ),
+                ],
               ),
-            ),
-            ListTile(
-              onTap: () {
-                // Navigator.of(context).pushReplacement(MaterialPageRoute(
-                //     builder: (context) => const HomeScreen()));
-              },
-              title: TextWidget(
-                text: 'Navigation',
-                fontSize: 14,
-                fontFamily: 'Bold',
-              ),
-            ),
-            ListTile(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: const Text(
-                            'Logout Confirmation',
-                            style: TextStyle(
-                                fontFamily: 'QBold',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          content: const Text(
-                            'Are you sure you want to Logout?',
-                            style: TextStyle(fontFamily: 'QRegular'),
-                          ),
-                          actions: <Widget>[
-                            MaterialButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: const Text(
-                                'Close',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            MaterialButton(
-                              onPressed: () async {
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const LoginPage()));
-                              },
-                              child: const Text(
-                                'Continue',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        ));
-              },
-              title: TextWidget(
-                text: 'Logout',
-                fontSize: 14,
-                fontFamily: 'Bold',
-              ),
-            ),
-          ],
-        ),
-      )),
-    );
+            )),
+          );
+        });
   }
 }
